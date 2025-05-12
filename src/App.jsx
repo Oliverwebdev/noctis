4c69154813111450b4cf0f450080a5dc130fb144
Hack3 <oliver@Hac3r>
2025-05-12 17:24:03 +0200

    again fix vite.config + package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Landing from './sites/Landing';
 import Mission from './sites/Mission';
 import Beyond from './sites/Beyond';
-import Landing from './sites/Landing';
-import Navbar from './components/Navbar'; // Import the new Navbar
+import './App.css';
 
-function App() {
-    return (
-        <Router>
-            <Navbar /> {/* Add the Navbar component */}
-            <Routes>
-                <Route path="/" element={<Landing />} />
-                <Route path="/mission" element={<Mission />} />
-                <Route path="/beyond" element={<Beyond />} />
-            </Routes>
-        </Router>
-    );
-}
+const App = () => {
+  return (
+    <BrowserRouter basename="/noctis">
+      <Navbar />
+      <div className="page-content">
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/mission" element={<Mission />} />
+          <Route path="/beyond" element={<Beyond />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
+
